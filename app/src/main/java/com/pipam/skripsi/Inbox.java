67dd620601c98cc61a5d9c1743b25a6027e59fd2
irfanrinaldy <ipank1412@gmail.com>
2016-12-22 00:03:00 +0800

    Commit Pertama

--- app/src/main/java/com/pipam/skripsi/Inbox.java
+++ app/src/main/java/com/pipam/skripsi/Inbox.java
@@ -1,34 +1,115 @@
 package com.pipam.skripsi;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 
 public class Inbox extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+    String[] m;
+    ListView listView;
+    String pesanTerpilih;
+
+    TextView lblMsg, lblNo;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbox);
+        listView = (ListView) findViewById(R.id.listViewSMS);
+        Uri uriSMSURI = Uri.parse("content://sms/inbox");
+        final String[] reqCols = new String[]{"address", "body"};
+        Cursor cursor = getContentResolver().query(uriSMSURI, null, null, null, null);
+        int[] to = new int[]{R.id.pengirim,R.id.isi};
+        SimpleCursorAdapter simpleCursorAdapter;
+        simpleCursorAdapter = new SimpleCursorAdapter(this, R.layout.content_inbox, cursor, reqCols, to,0);
+        simpleCursorAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                //ubah nomor dengan contact name
+                if (columnIndex == 2) {
+
+                    TextView tvContact = (TextView) view;
+//                    TextView tvMessage = (TextView) view;
+                    String pengirimDB = cursor.getString(cursor.getColumnIndex("address"));
+//                    String pesanDB = cursor.getString(cursor.getColumnIndex("message"));
+
+                    //get contact name
+                    Uri contactUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(pengirimDB));
+                    Cursor cursor1 = getContentResolver().query(contactUri, null, null, null, null);
+                    ContentResolver contentResolver = getContentResolver();
+
+                    int size = cursor1.getCount();
+                    if (size > 0 && cursor1 != null) {
+                        Log.d("SMS", "masuk seleksi");
+                        for (int i = 0; i < size; i++) {
+                            cursor1.moveToPosition(i);
+                            String id1 = cursor1.getString(cursor1.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+                            Log.d("SMS", id1);
+                            Cursor phoneCursor = contentResolver.query(contactUri, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " =? ", new String[]{id1}, null);
+
+                            if (phoneCursor.moveToFirst()) {
+                                String namaKontak = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+//                                String isiPesan = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DATA.toLowerCase()));
+                                phoneCursor.close();
+                                tvContact.setText(namaKontak);
+                            } else {
+                                tvContact.setText(pengirimDB);
+//                                tvMessage.setText(pesanDB);
+                            }
+
+                        }
+                        cursor1.close();
+                    } else {
+                        tvContact.setText(pengirimDB);
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+        listView.setAdapter(simpleCursorAdapter);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-             New.start(Inbox.this);
+            public void onClick(View v) {
+                New.start(Inbox.this);
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -55,5 +136,41 @@ public class Inbox extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Inbox Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
--- app/src/main/java/com/pipam/skripsi/New.java
+++ app/src/main/java/com/pipam/skripsi/New.java
@@ -1,26 +1,115 @@
 package com.pipam.skripsi;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * Created by Pipam on 10/28/2016.
- */
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class New extends AppCompatActivity {
+    EditText nomorkontak, text;
+
+    private static final int CONTACT_PICKER_RESULT = 1001;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     public static void start(Context context) {
         Intent intent = new Intent(context, New.class);
         context.startActivity(intent);
     }
 
+    public void doLaunchContactPicker(View view) {
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, uri);
+        startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String phone = "";
+        Cursor contacts = null;
+        try {
+            if (resultCode == RESULT_OK) {
+                switch (requestCode) {
+                    case CONTACT_PICKER_RESULT:
+                        Uri result = data.getData();
+                        String id = result.getLastPathSegment();
+                        contacts = getContentResolver().query(
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                                ContactsContract.CommonDataKinds.Phone._ID + "=?", new String[]{id}, null);
+                        int phoneIdx = contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DATA);
+                        if (contacts.moveToFirst()) {
+                            phone = contacts.getString(phoneIdx);
+                            EditText phoneTxt = (EditText) findViewById(R.id.editTextPhoneNo);
+                            phoneTxt.setText(phone);
+                        } else {
+                            Toast.makeText(this, "error", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+                }
+            } else {
+                Toast.makeText(New.this, "Belum dipilih",
+                        Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            if (contacts != null) {
+                contacts.close();
+            }
+        }
+    }
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+        Button send = (Button) findViewById(R.id.buttonSend);
+        nomorkontak = (EditText) findViewById(R.id.editTextPhoneNo);
+        Intent intent = getIntent();
+        if (intent.getStringExtra("message") != null) {
+            text.setText(intent.getStringExtra("message"));
+        }
+        send.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String pesan = text.getText().toString();
+                String nomor = nomorkontak.getText().toString();
+                if (pesan.length() > 0 && nomor.length() > 0) {
+                    try {
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(nomor, null, pesan, null, null);
+                        ContentValues values = new ContentValues();
+                        values.put("address", nomor);
+                        values.put("body", pesan);
+                        getContentResolver().insert(
+                                Uri.parse("content://sms/sent"), values);
+                        Toast.makeText(New.this,
+                                "Pesan berhasil dikirim", Toast.LENGTH_SHORT)
+                                .show();
+                        finish();
+                    } catch (Exception e) {
+                        Toast.makeText(New.this,
+                                "Nomor atau isi pesan masih kosong",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(New.this,
+                            "Nomor atau isi pesan masih kosong",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
-
